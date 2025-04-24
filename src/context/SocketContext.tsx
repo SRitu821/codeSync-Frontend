d26226e8b80047d18d16a529ca9ce3148c4adab7
Ritu Singh <157214936+SRitu821@users.noreply.github.com>
2025-04-24 23:05:44 +0530

    Update SocketContext.tsx

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -28,7 +28,7 @@ export const useSocket = (): SocketContextType => {
 }
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
-
+console.log("BACKEND_URL",BACKEND_URL)
 const SocketProvider = ({ children }: { children: ReactNode }) => {
     const {
         users,
